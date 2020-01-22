617909c85383ccecc10ae414e21bb0759770aeaf
Chen <liangch0505@gmail.com>
2020-01-21 19:57:00 -0600

    webcam support

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from modules import *
-video_stream=VideoStream(show_img_flag=False,debug_mode=False)
+video_stream=VideoStream(input_source=0,debug_mode=False)
 video_stream.start()
 
 
 #Example:
 import cv2
 import time
-time.sleep(3)
 
+time.sleep(3)
 while True:
-    color_image,depth_image=video_stream.get_frames()
-    cv2.imshow('color_image',color_image)
+    #color_image,depth_image=video_stream.get_frames()
+    #color_image=video_stream.get_frames()
+    cv2.imshow('color_image',video_stream.frame)
     cv2.waitKey(1)
