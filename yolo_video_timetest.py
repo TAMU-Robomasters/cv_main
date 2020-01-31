6751d8b2e2595419e1bbc0dfe4f1eba38d370f2e
Bennett <bennettacarley@gmail.com>
2020-01-30 21:25:48 -0600

    Fixed?

--- yolo_video_timetest.py
+++ yolo_video_timetest.py
@@ -36,7 +36,7 @@ def modeling(inputpath,ioutput,yolo,iconfidence,ithreshold):
     # initialize a list of colors to represent each possible class label
     np.random.seed(42)
     COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
-    	dtype="uint8")
+        dtype="uint8")
 
     # derive the paths to the YOLO weights and model configuration
     weightsPath = os.path.sep.join([yolo, weights_fname])
@@ -58,87 +58,87 @@ def modeling(inputpath,ioutput,yolo,iconfidence,ithreshold):
 
     # try to determine the total number of frames in the video file
     try:
-    	#prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() else cv2.CAP_PROP_FRAME_COUNT
-    	total = int(vs.get(cv2.CV_CAP_PROP_FRAME_COUNT))
-    	print("[INFO] {} total frames in video".format(total))
+        #prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() else cv2.CAP_PROP_FRAME_COUNT
+        total = int(vs.get(cv2.CV_CAP_PROP_FRAME_COUNT))
+        print("[INFO] {} total frames in video".format(total))
 
     # an error occurred while trying to determine the total
     # number of frames in the video file
     except:
-    	print("[INFO] could not determine # of frames in video")
-    	print("[INFO] no approx. completion time can be provided")
-    	total = -1
+        print("[INFO] could not determine # of frames in video")
+        print("[INFO] no approx. completion time can be provided")
+        total = -1
 
     # loop over frames from the video file stream
     counter = 0
     while True:
-    	# read the next frame from the file
-    	(grabbed, frame) = vs.read()
+        # read the next frame from the file
+        (grabbed, frame) = vs.read()
 
-    	# if the frame was not grabbed, then we have reached the end
-    	# of the stream
-    	if not grabbed:
-    		break
+        # if the frame was not grabbed, then we have reached the end
+        # of the stream
+        if not grabbed:
+            break
 
-    	# if the frame dimensions are empty, grab them
-    	if W is None or H is None:
-    		(H, W) = frame.shape[:2]
-        	# construct a blob from the input frame and then perform a forward
-    	# pass of the YOLO object detector, giving us our bounding boxes
-    	# and associated probabilities
-    	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-    		swapRB=True, crop=False)
-    	net.setInput(blob)
-    	start = time.time()
-    	layerOutputs = net.forward(ln)
-    	end = time.time()
+        # if the frame dimensions are empty, grab them
+        if W is None or H is None:
+            (H, W) = frame.shape[:2]
+            # construct a blob from the input frame and then perform a forward
+        # pass of the YOLO object detector, giving us our bounding boxes
+        # and associated probabilities
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+            swapRB=True, crop=False)
+        net.setInput(blob)
+        start = time.time()
+        layerOutputs = net.forward(ln)
+        end = time.time()
 
-    	# initialize our lists of detected bounding boxes, confidences,
-    	# and class IDs, respectively
-    	boxes = []
-    	confidences = []
-    	classIDs = []
-        	# loop over each of the layer outputs
-    	for output in layerOutputs:
-    		# loop over each of the detections
-    		for detection in output:
-    			# extract the class ID and confidence (i.e., probability)
-    			# of the current object detection
-    			scores = detection[5:]
-    			classID = np.argmax(scores)
-    			confidence = scores[classID]
+        # initialize our lists of detected bounding boxes, confidences,
+        # and class IDs, respectively
+        boxes = []
+        confidences = []
+        classIDs = []
+            # loop over each of the layer outputs
+        for output in layerOutputs:
+            # loop over each of the detections
+            for detection in output:
+                # extract the class ID and confidence (i.e., probability)
+                # of the current object detection
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
 
-    			# filter out weak predictions by ensuring the detected
-    			# probability is greater than the minimum probability
-    			if confidence > iconfidence:
-    				# scale the bounding box coordinates back relative to
-    				# the size of the image, keeping in mind that YOLO
-    				# actually returns the center (x, y)-coordinates of
-    				# the bounding box followed by the boxes' width and
-    				# height
-    				box = detection[0:4] * np.array([W, H, W, H])
-    				(centerX, centerY, width, height) = box.astype("int")
+                # filter out weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > iconfidence:
+                    # scale the bounding box coordinates back relative to
+                    # the size of the image, keeping in mind that YOLO
+                    # actually returns the center (x, y)-coordinates of
+                    # the bounding box followed by the boxes' width and
+                    # height
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
 
-    				# use the center (x, y)-coordinates to derive the top
-    				# and and left corner of the bounding box
-    				x = int(centerX - (width / 2))
-    				y = int(centerY - (height / 2))
+                    # use the center (x, y)-coordinates to derive the top
+                    # and and left corner of the bounding box
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
 
-    				# update our list of bounding box coordinates,
-    				# confidences, and class IDs
-    				boxes.append([x, y, int(width), int(height)])
+                    # update our list of bounding box coordinates,
+                    # confidences, and class IDs
+                    boxes.append([x, y, int(width), int(height)])
 
-    				confidences.append(float(confidence))
-    				classIDs.append(classID)
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
 
-            	# apply non-maxima suppression to suppress weak, overlapping
-    	# bounding boxes
-    	idxs = cv2.dnn.NMSBoxes(boxes, confidences, iconfidence,ithreshold)
+                # apply non-maxima suppression to suppress weak, overlapping
+        # bounding boxes
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, iconfidence,ithreshold)
 
-    	# ensure at least one detection exists
-    	if len(idxs) > 0:
-    		# loop over the indexes we are keeping
-    		for i in idxs.flatten():
+        # ensure at least one detection exists
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
                 # extract the bounding box coordinates
                 (x, y) = (boxes[i][0], boxes[i][1])
                 (w, h) = (boxes[i][2], boxes[i][3])
@@ -149,7 +149,7 @@ def modeling(inputpath,ioutput,yolo,iconfidence,ithreshold):
                 cv2.putText(frame, text, (x, y - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
         # check if the video writer is None
         if writer is None:
-    		# initialize our video writer
+            # initialize our video writer
             fourcc = cv2.VideoWriter_fourcc(*"MJPG")
             writer = cv2.VideoWriter(ioutput, fourcc, 30, (frame.shape[1], frame.shape[0]), True)
 
