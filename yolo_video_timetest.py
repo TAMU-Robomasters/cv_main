bd3a329b722c668f0df60219cedc43da97cfee12
Anooj Shah <thisisanooj@gmail.com>
2020-01-30 21:02:11 -0600

    FIX SPACES PLZ

--- yolo_video_timetest.py
+++ yolo_video_timetest.py
@@ -1,5 +1,6 @@
 # import the necessary packages
 import numpy as np
+import tracker
 import argparse
 import time
 import cv2
@@ -12,8 +13,8 @@ def modeling(inputpath,ioutput,yolo,iconfidence,ithreshold):
     weights_fname='v3t.weights'
     cfg_fname='v3t.cfg'
     classes_fname='v3t.names'
-    
-    
+
+
     # construct the argument parse and parse the arguments
 #    ap = argparse.ArgumentParser()
 #    ap.add_argument("-i", "--input", required=True,
@@ -27,57 +28,58 @@ def modeling(inputpath,ioutput,yolo,iconfidence,ithreshold):
 #    ap.add_argument("-t", "--threshold", type=float, default=0.3,
 #    	help="threshold when applyong non-maxima suppression")
 #    args = vars(ap.parse_args())
-#    
+#
     # load the COCO class labels our YOLO model was trained on
     labelsPath = os.path.sep.join([yolo, classes_fname])
     LABELS = open(labelsPath).read().strip().split("\n")
-    
+
     # initialize a list of colors to represent each possible class label
     np.random.seed(42)
     COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
     	dtype="uint8")
-    
+
     # derive the paths to the YOLO weights and model configuration
     weightsPath = os.path.sep.join([yolo, weights_fname])
     configPath = os.path.sep.join([yolo, cfg_fname])
-    
+
     # load our YOLO object detector trained on COCO dataset (80 classes)
     # and determine only the *output* layer names that we need from YOLO
     print("[INFO] loading YOLO from disk...")
     net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
     ln = net.getLayerNames()
     ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-    
+
     # initialize the video stream, pointer to output video file, and
     # frame dimensions
+
     vs = cv2.VideoCapture(inputpath)
     writer = None
     (W, H) = (None, None)
-    
+
     # try to determine the total number of frames in the video file
     try:
     	#prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() else cv2.CAP_PROP_FRAME_COUNT
     	total = int(vs.get(cv2.CV_CAP_PROP_FRAME_COUNT))
     	print("[INFO] {} total frames in video".format(total))
-    
+
     # an error occurred while trying to determine the total
     # number of frames in the video file
     except:
     	print("[INFO] could not determine # of frames in video")
     	print("[INFO] no approx. completion time can be provided")
     	total = -1
-    
+
     # loop over frames from the video file stream
     counter = 0
     while True:
     	# read the next frame from the file
     	(grabbed, frame) = vs.read()
-    
+
     	# if the frame was not grabbed, then we have reached the end
     	# of the stream
     	if not grabbed:
     		break
-    
+
     	# if the frame dimensions are empty, grab them
     	if W is None or H is None:
     		(H, W) = frame.shape[:2]
@@ -90,7 +92,7 @@ def modeling(inputpath,ioutput,yolo,iconfidence,ithreshold):
     	start = time.time()
     	layerOutputs = net.forward(ln)
     	end = time.time()
-    
+
     	# initialize our lists of detected bounding boxes, confidences,
     	# and class IDs, respectively
     	boxes = []
@@ -105,7 +107,7 @@ def modeling(inputpath,ioutput,yolo,iconfidence,ithreshold):
     			scores = detection[5:]
     			classID = np.argmax(scores)
     			confidence = scores[classID]
-    
+
     			# filter out weak predictions by ensuring the detected
     			# probability is greater than the minimum probability
     			if confidence > iconfidence:
@@ -116,61 +118,58 @@ def modeling(inputpath,ioutput,yolo,iconfidence,ithreshold):
     				# height
     				box = detection[0:4] * np.array([W, H, W, H])
     				(centerX, centerY, width, height) = box.astype("int")
-    
+
     				# use the center (x, y)-coordinates to derive the top
     				# and and left corner of the bounding box
     				x = int(centerX - (width / 2))
     				y = int(centerY - (height / 2))
-    
+
     				# update our list of bounding box coordinates,
     				# confidences, and class IDs
     				boxes.append([x, y, int(width), int(height)])
+
     				confidences.append(float(confidence))
     				classIDs.append(classID)
-   
+
             	# apply non-maxima suppression to suppress weak, overlapping
     	# bounding boxes
-    	idxs = cv2.dnn.NMSBoxes(boxes, confidences, iconfidence,
-    		ithreshold)
-    
+    	idxs = cv2.dnn.NMSBoxes(boxes, confidences, iconfidence,ithreshold)
+
     	# ensure at least one detection exists
     	if len(idxs) > 0:
     		# loop over the indexes we are keeping
     		for i in idxs.flatten():
-    			# extract the bounding box coordinates
-    			(x, y) = (boxes[i][0], boxes[i][1])
-    			(w, h) = (boxes[i][2], boxes[i][3])              
-    			# draw a bounding box rectangle and label on the frame
-    			color = [int(c) for c in COLORS[classIDs[i]]]
-    			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-    			text = "{}: {:.4f}".format(LABELS[classIDs[i]],
-    				confidences[i])
-    			cv2.putText(frame, text, (x, y - 5),
-    				cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-        	# check if the video writer is None
-    	if writer is None:
+                # extract the bounding box coordinates
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+                # draw a bounding box rectangle and label on the frame
+                color = [int(c) for c in COLORS[classIDs[i]]]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                text = "{}: {:.4f}".format(LABELS[classIDs[i]],confidences[i])
+                cv2.putText(frame, text, (x, y - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        # check if the video writer is None
+        if writer is None:
     		# initialize our video writer
-    		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    		writer = cv2.VideoWriter(ioutput, fourcc, 30,
-    			(frame.shape[1], frame.shape[0]), True)
-    
-    		# some information on processing single frame
-    		if total > 0:
-    			elap = (end - start)
-    			print("[INFO] single frame took {:.4f} seconds".format(elap))
-    			print("[INFO] estimated total time to finish: {:.4f}".format(
-    				elap * total))
-    	timenow = datetime.now()
-    	current_time = timenow.strftime("%H:%M:%S")
-    	print(current_time)
-    	counter +=1
-    	print(counter)
-    	print(boxes,"\n")
-    	# write the output frame to disk
-    	writer.write(frame)
-    
+            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            writer = cv2.VideoWriter(ioutput, fourcc, 30, (frame.shape[1], frame.shape[0]), True)
+
+            # some information on processing single frame
+            if total > 0:
+                elap = (end - start)
+                print("[INFO] single frame took {:.4f} seconds".format(elap))
+                print("[INFO] estimated total time to finish: {:.4f}".format(elap * total))
+        timenow = datetime.now()
+        current_time = timenow.strftime("%H:%M:%S")
+        print(current_time)
+        counter +=1
+        print(counter)
+        print(boxes,"\n")
+        print(tracker.init(frame,bboxes))
+
+        # write the output frame to disk
+        writer.write(frame)
+
     # release the file pointers
     print("[INFO] cleaning up...")
     writer.release()
     vs.release()
-    
