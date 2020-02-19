6ad8f78144d99a6b1a65b9318b9011fa65033ceb
jeff-hykin <jeff.hykin@gmail.com>
2020-02-19 17:24:12 -0600

    add windows

--- scripts/run.rb
+++ scripts/run.rb
@@ -10,6 +10,22 @@ end
 
 if OS.is?(:linux)
     exec("sudo", $paths['project_bin']/first_argument, *other_arguments )
-else
+elsif OS.is?(:mac)
     exec($paths['project_bin']/first_argument, *other_arguments )
+else
+    command = <<-HEREDOC.remove_indent
+        docker run \
+            -it \
+            --rm \
+            --network=host \
+            -v "$PROJECT_PATH":/project \
+            -v /var/run/docker.sock:/var/run/docker.sock \
+            -v /usr/local/bin/docker:/usr/local/bin/docker \
+            --env PATH="/project/project_bin:${PATH}" \
+            -- \
+            ubuntu:bionic-20200112
+    HEREDOC
+    puts command
+    puts ""
+    puts "The command (above) still needs to be converted to windows"
 end
