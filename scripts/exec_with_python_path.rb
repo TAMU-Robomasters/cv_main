a38be23fb432c125b7b6b9c1acc9262fdd0a4903
jeff-hykin <jeff.hykin@gmail.com>
2020-02-22 23:18:14 -0600

    - remove unneeded path

--- scripts/exec_with_python_path.rb
+++ scripts/exec_with_python_path.rb
@@ -4,7 +4,6 @@ require_relative Info.paths["ruby_tools"] # corrisponds to the "(path)" inside i
 # add the project to PYTHONPATH
 old_python_path = ENV["PYTHONPATH"]
 project_path = $paths["project_root"]
-source_path = $paths["source_dir"]
-ENV["PYTHONPATH"] = "#{old_python_path}:#{project_path}:#{source_path}"
+ENV["PYTHONPATH"] = "#{old_python_path}:#{project_path}"
 # run the python command
 exec("python", *ARGV)
