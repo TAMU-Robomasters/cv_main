afe63e4f92e581f5b6a73feb33b5fa441a438d27
Jeff Hykin <jeff.hykin@gmail.com>
2020-02-28 11:38:17 -0600

    add data to receive from embedded

---
+++ source/embedded_communication/receive_from_embedded.py
@@ -0,0 +1,10 @@
+# summary
+#    this file should listen to events from the the driver and activate code accordingly
+#    this file will likely need to be called/initilized by main.py
+
+def driver_activated_aim_assist():
+    pass
+
+def driver_activated_rune_challengen():
+    pass
+
