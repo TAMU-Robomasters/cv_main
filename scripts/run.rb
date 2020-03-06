8152b36333afd619c85087c69df7c5795616556b
jeff-hykin <jeff.hykin@gmail.com>
2020-03-06 07:01:57 -0600

    - add windows run script

--- scripts/run.rb
+++ scripts/run.rb
@@ -13,21 +13,22 @@ if OS.is?(:linux)
 elsif OS.is?(:mac)
     exec($paths['project_bin']/first_argument, *other_arguments )
 else
-    command = <<-HEREDOC.remove_indent
-        docker run \
-            -it \
-            --rm \
-            --network=host \
-            -v "#{Info.folder}":/project \
-            -v /var/run/docker.sock:/var/run/docker.sock \
-            -v /usr/local/bin/docker:/usr/local/bin/docker \
-            --env PATH="/project/project_bin:${PATH}" \
-            -- \
-            ubuntu:bionic-20200112
-    HEREDOC
-    puts command
-    exec(command)
-    puts ""
-    # FIXME
-    puts "The command (above) still needs to be converted to windows"
+    command = [
+        'docker',
+        'run',
+        '-it',
+        '--rm',
+        '--network=host',
+        '-v', "#{Info.folder}:/project",
+        '--workdir', '/project/ ',
+        '-v', '//var/run/docker.sock:/var/run/docker.sock',
+        '-v','/usr/local/bin/docker:/usr/local/bin/docker',
+        '--',
+        LocalDocker.new("main").image_name,
+        *Console.args
+    ]
+    # helpful sources: https://stackoverflow.com/questions/36765138/bind-to-docker-socket-on-windows
+    puts command.join(" ")
+    # TODO: test this on windows
+    exec(*command)
 end
