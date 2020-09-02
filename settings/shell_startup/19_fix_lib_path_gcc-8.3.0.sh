61df801581e34ae15e813c2a78ae0dd2eee04beb
Jeff Hykin <jeff.hykin@gmail.com>
2020-09-02 12:37:23 -0500

    -

---
+++ settings/shell_startup/19_fix_lib_path_gcc-8.3.0.sh
@@ -0,0 +1,3 @@
+gcc_path="$(find -L /nix/store/ -name gcc-8.3.0-lib/lib)"
+echo "gcc_path: $gcc_path"
+# export LD_LIBRARY_PATH="/nix/store/784rh7jrfhagbkydjfrv68h9x3g4gqmk-gcc-8.3.0-lib/lib:$LD_LIBRARY_PATH"
