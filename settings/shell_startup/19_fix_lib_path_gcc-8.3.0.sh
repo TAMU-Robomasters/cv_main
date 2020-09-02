93bd83c2e85011b638ad8efe64e5e31cb19f6ce4
Jeff Hykin <jeff.hykin@gmail.com>
2020-09-02 12:50:47 -0500

    -

--- settings/shell_startup/19_fix_lib_path_gcc-8.3.0.sh
+++ settings/shell_startup/19_fix_lib_path_gcc-8.3.0.sh
@@ -1,5 +1,10 @@
 gcc_cpp_lib_path="$(find -L /nix/store/ -name libstdc++.so.6 | grep -e 'gcc-8.3.0-lib/lib' | head -n 1)"
-echo "gcc_path: $gcc_cpp_lib_path"
-gcc_lib_path="$(basename "$gcc_cpp_lib_path")"
-echo "gcc_lib_path: $gcc_lib_path"
-# export LD_LIBRARY_PATH="/nix/store/784rh7jrfhagbkydjfrv68h9x3g4gqmk-gcc-8.3.0-lib/lib:$LD_LIBRARY_PATH"
+# if the file exists (which it should linux)
+if [[ -f "$gcc_cpp_lib_path" ]] 
+then
+    # get the lib
+    gcc_lib_path="$(dirname "$gcc_cpp_lib_path")"
+    # update teh variable
+    export LD_LIBRARY_PATH="$gcc_lib_path:$LD_LIBRARY_PATH"
+fi
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
