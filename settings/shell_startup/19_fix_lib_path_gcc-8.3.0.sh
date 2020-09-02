c730a7e1a3f7c6683ecb0432e6e9627bcbb6e83f
Jeff Hykin <jeff.hykin@gmail.com>
2020-09-02 12:43:52 -0500

    -

--- settings/shell_startup/19_fix_lib_path_gcc-8.3.0.sh
+++ settings/shell_startup/19_fix_lib_path_gcc-8.3.0.sh
@@ -1,3 +1,5 @@
-gcc_path="$(find -L /nix/store/ -name gcc-8.3.0-lib/lib)"
-echo "gcc_path: $gcc_path"
+gcc_cpp_lib_path="$(find -L /nix/store/ -name libstdc++.so.6 | grep -e 'gcc-8.3.0-lib/lib' | head -n 1)"
+echo "gcc_path: $gcc_cpp_lib_path"
+gcc_lib_path="$(basename "$gcc_cpp_lib_path")"
+echo "gcc_lib_path: $gcc_lib_path"
 # export LD_LIBRARY_PATH="/nix/store/784rh7jrfhagbkydjfrv68h9x3g4gqmk-gcc-8.3.0-lib/lib:$LD_LIBRARY_PATH"
