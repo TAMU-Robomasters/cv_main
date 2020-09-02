27532e69eafb75b550d78d14608e7235038f786d
Jeff Hykin <jeff.hykin@gmail.com>
2020-09-02 14:34:49 -0500

    -

--- settings/shell_startup/19_fix_lib_paths_for_opencv.sh
+++ settings/shell_startup/19_fix_lib_paths_for_opencv.sh
@@ -2,7 +2,7 @@
 # libstdc++.so.6 fix
 # 
 name_of_check="libstdcpp_so_6_fix"
-which_gcc_to_look_for="gcc-8.3.0-lib"
+which_package_look_for="gcc-8.3.0-lib"
 location_of_file_cache="./settings/.cache/.$name_of_check.cleanable"
 # if it hasnt cached it
 if ! [[ -f "$location_of_file_cache" ]]; then
@@ -10,7 +10,7 @@ if ! [[ -f "$location_of_file_cache" ]]; then
     mkdir -p "$(dirname "$location_of_file_cache")"
     
     # find the first gcc package with the missing libstdc++.so.6
-    lib_path="$(find -L /nix/store/ -name libstdc++.so.6 2>/dev/null | grep -e "$which_gcc_to_look_for/lib" | head -n 1)"
+    lib_path="$(find -L /nix/store/ -name libstdc++.so.6 2>/dev/null | grep -e "$which_package_look_for/lib" | head -n 1)"
     
     
     # save the result to a file (because that^ operation takes awhile)
@@ -36,7 +36,7 @@ fi
 # libSM fix
 # 
 name_of_check="libSM"
-which_gcc_to_look_for="libSM-1.2.3"
+which_package_look_for="libSM-1.2.3"
 location_of_file_cache="./settings/.cache/.$name_of_check.cleanable"
 # if it hasnt cached it
 if ! [[ -f "$location_of_file_cache" ]]; then
@@ -44,7 +44,7 @@ if ! [[ -f "$location_of_file_cache" ]]; then
     mkdir -p "$(dirname "$location_of_file_cache")"
     
     # find the first package with the missing lib
-    lib_path="$(find -L /nix/store/ -name libSM.so.6 2>/dev/null | grep -e "$which_gcc_to_look_for/lib" | head -n 1)"
+    lib_path="$(find -L /nix/store/ -name libSM.so.6 2>/dev/null | grep -e "$which_package_look_for/lib" | head -n 1)"
     
     # save the result to a file (because that^ operation takes awhile)
     echo "$lib_path" > "$location_of_file_cache"
@@ -60,7 +60,36 @@ then
     
     # update the library variable
     export LD_LIBRARY_PATH="$lib_dir:$LD_LIBRARY_PATH"
+fi
+
+# 
+# libICE.so.6 fix
+# 
+name_of_check="libICE"
+which_package_look_for="libICE"
+location_of_file_cache="./settings/.cache/.$name_of_check.cleanable"
+# if it hasnt cached it
+if ! [[ -f "$location_of_file_cache" ]]; then
+    # make sure the folder exists
+    mkdir -p "$(dirname "$location_of_file_cache")"
     
+    # find the first package with the missing lib
+    lib_path="$(find -L /nix/store/ -name libICE.so.6 2>/dev/null | head -n 1)"
+    
+    # save the result to a file (because that^ operation takes awhile)
+    echo "$lib_path" > "$location_of_file_cache"
+    
+fi
+# load the path from the cache
+lib_path="$(cat "$location_of_file_cache")"
+# if the file exists (which it should linux)
+if [[ -f "$lib_path" ]] 
+then
+    # get the directory
+    lib_dir="$(dirname "$lib_path")"
+    
+    # update the library variable
+    export LD_LIBRARY_PATH="$lib_dir:$LD_LIBRARY_PATH"
 fi
 
 # 
