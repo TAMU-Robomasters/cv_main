a3e591127f9a0b980d615e792475989286032324
Jeff Hykin <jeff.hykin@gmail.com>
2020-09-06 18:33:20 -0500

    -

--- settings/shell_startup/22_setup_pythonpath.sh
+++ settings/shell_startup/22_setup_pythonpath.sh
@@ -1,12 +1,2 @@
-# 
 # add this project folder to the python path
-# 
-if [ -z "$PYTHONPATH" ]
-then
-    # pythonpath is empty, just set to to PWD
-    PYTHONPATH="$PWD"
-      echo "\$var is empty"
-else
-    # otherwise put it after the other modules
-    PYTHONPATH="$PYTHONPATH:$PWD"
-fi
+export PYTHONPATH="$PYTHONPATH:$PWD"
