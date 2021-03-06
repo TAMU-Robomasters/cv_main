de57057a5123afb7986197e3e433e1b0add94bb1
Vatsal Mistry <vatsalmistry4796@gmail.com>
2021-03-06 12:24:57 -0600

    updated record script

--- source/modeling/_tests/record_video.py
+++ source/modeling/_tests/record_video.py
@@ -2,6 +2,7 @@ import time
 import pyrealsense2 as rs
 import numpy as np
 import sys
+import cv2
 # relative imports
 from toolbox.video_tools import Video
 from toolbox.image_tools import Image
@@ -13,6 +14,7 @@ framerate = PARAMETERS['aiming']['stream_framerate']
 gridSize = PARAMETERS['aiming']['grid_size']
 frameAmount = PARAMETERS['videostream']['testing']['camera_frames']
 timeRecord = PARAMETERS['videostream']['testing']['record_time']
+videoLocation = PATHS['record_video_output']
 
 pipeline = rs.pipeline()                                            
 config = rs.config()                                                
@@ -20,21 +22,19 @@ config.enable_stream(rs.stream.depth, streamWidth, streamHeight, rs.format.z16, 
 config.enable_stream(rs.stream.color, streamWidth, streamHeight, rs.format.bgr8, framerate) 
 pipeline.start(config)
 
+frame_height, frame_width = 480,848
+frame_dimensions = (frame_width, frame_height)
+colorwriter = cv2.VideoWriter(videoLocation, cv2.VideoWriter_fourcc(*'mp4v'), 30, frame_dimensions)
+
 t0 = time.time()
-framesList = []
-status = True
-
-while (time.time()-t0) <timeRecord:
-    frames = pipeline.wait_for_frames()     
-    color_frame = frames.get_color_frame()  
-
-    if not color_frame:
-        print("INVALID FRAME, SKIPPING")
-        continue
-
-    color_image = np.asanyarray(color_frame.get_data())
-    image = Image(color_image) 
-    framesList.append(image.img)
-
-if status:
-    Video.create_from_frames(framesList, save_to=PATHS["record_video_output"],fps=framerate)
+try:
+    while (time.time()-t0)<timeRecord:
+        frames = pipeline.wait_for_frames()
+        color_frame = frames.get_color_frame()
+        if not color_frame:
+            continue
+        color_image = np.asanyarray(color_frame.get_data())
+        colorwriter.write(color_image)
+finally:
+    colorwriter.release()
+    pipeline.stop()
