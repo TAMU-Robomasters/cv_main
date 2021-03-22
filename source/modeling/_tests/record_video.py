74e2997ad9e3fe339e8dfee2735ecddea4147c70
Vatsal Mistry <vatsalmistry4796@gmail.com>
2021-03-22 18:31:22 -0500

    added depth recording feature

--- source/modeling/_tests/record_video.py
+++ source/modeling/_tests/record_video.py
@@ -14,7 +14,9 @@ framerate = PARAMETERS['aiming']['stream_framerate']
 gridSize = PARAMETERS['aiming']['grid_size']
 frameAmount = PARAMETERS['videostream']['testing']['camera_frames']
 timeRecord = PARAMETERS['videostream']['testing']['record_time']
-videoLocation = PATHS['record_video_output']
+colorVideoLocation = PATHS['record_video_output_color']
+depthVideoLocation = PATHS['record_video_output_depth']
+
 
 pipeline = rs.pipeline()                                            
 config = rs.config()                                                
@@ -23,17 +25,24 @@ config.enable_stream(rs.stream.color, streamWidth, streamHeight, rs.format.bgr8,
 pipeline.start(config)
 
 frame_dimensions = (streamWidth, streamHeight)
-colorwriter = cv2.VideoWriter(videoLocation, cv2.VideoWriter_fourcc(*'mp4v'), framerate, frame_dimensions)
+colorwriter = cv2.VideoWriter(colorVideoLocation, cv2.VideoWriter_fourcc(*'mp4v'), framerate, frame_dimensions)
+depthwriter = cv2.VideoWriter(depthVideoLocation, cv2.VideoWriter_fourcc(*'mp4v'), framerate, frame_dimensions)
 
 t0 = time.time()
 try:
     while (time.time()-t0)<timeRecord:
         frames = pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
-        if not color_frame:
+        depth_frame = frames.get_color_frame()
+
+        if not color_frame or not depth_frame:
             continue
+
         color_image = np.asanyarray(color_frame.get_data())
+        depth_image = np.asanyarray(depth_frame.get_data())
         colorwriter.write(color_image)
+        depthwriter.write(color_image)
 finally:
     colorwriter.release()
+    depthwriter.release()
     pipeline.stop()
