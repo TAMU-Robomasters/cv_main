c0a3eacc7ca89333a3e9c762531071161c1027ce
Jeff Hykin <jeff.hykin@gmail.com>
2021-04-20 14:33:01 -0500

    update the git hooks

--- settings/git/hooks/post-merge/00210_check_pip_modules.sh
+++ settings/git/hooks/post-merge/00210_check_pip_modules.sh
@@ -1,1 +1,5 @@
-"$PROJECTR_FOLDER/settings/commands/.check_pip_modules"
+# if PROJECTR_FOLDER is not set, then assume the current folder
+if [[ -z "$PROJECTR_FOLDER" ]]
+then
+    "$PROJECTR_FOLDER/settings/commands/.check_pip_modules"
+fi
