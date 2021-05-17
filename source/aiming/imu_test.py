6ac9bfbbbeb6271e43cf66f4132fa3825b48577d
Sai Juttu <saijuttu30@gmail.com>
2021-05-17 18:58:34 -0500

    imu data frames

--- source/aiming/imu_test.py
+++ source/aiming/imu_test.py
@@ -4,8 +4,9 @@ import cv2
 
 pipeline = rs.pipeline()                                            # declares and initializes the pipeline variable
 config = rs.config()                                                # declares and initializes the config variable for the pipeline
-config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 60)  # this starts the depth stream and sets the size and format
-config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 60) # this starts the color stream and set the size and format
+config.enable_stream(rs.stream.accel,rs.format.motion_xyz32f,200)
+config.enable_stream(rs.stream.gyro,rs.format.motion_xyz32f,200)
+config.enable_stream(rs.stream.pose,rs.format.motion_xyz32f,200)
 profile = pipeline.start(config)
 
 def DistanceInBox(bbox):
@@ -14,17 +15,9 @@ def DistanceInBox(bbox):
 
         while True: 
 
-            frames = pipeline.wait_for_frames()     # gets all frames
-            depth_frame = frames.get_depth_frame()  # gets the depth frame
-            color_frame = frames.get_color_frame()  # gets the color frame 
-            if not depth_frame or not color_frame:  # if there is no aligned_depth_frame or color_frame then leave the loop
-                continue
-
-            # we turn the depth and color frames into numpy arrays because we need to draw a rectangle and stack the two arrays
-            depth_image = np.asanyarray(depth_frame.get_data()) 
-
-            # causes 1FPS Drop
-            return getDistFromArray(depth_image, bbox)     # gets the distance for the normal depth image
+            frames = pipeline.wait_for_frames()     
+            motion_frame = frames.as_motion_frame().get_motion_data()
+            # do whatever with the frames
 
     finally:
         pipeline.stop()
