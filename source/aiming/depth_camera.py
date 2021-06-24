6b97bf0d47ea366bbbdd900fbd01f85d0ecc872b
MatsalVistry <vatsalmistry4796@gmail.com>
2021-06-23 20:57:07 -0500

    documented depth camera file

--- source/aiming/depth_camera.py
+++ source/aiming/depth_camera.py
@@ -11,56 +11,69 @@ stream_height = PARAMETERS['aiming']['stream_height']
 barrel_camera_gap = PARAMETERS['aiming']['barrel_camera_gap']
 
 def visualizeDepthFrame(depth_frame_array):
+    """
+    Displays a depth frame in a visualized color format.
+
+    Input: Depth Frame.
+    Output: None.
+    """
     depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_frame_array, alpha = 0.04), cv2.COLORMAP_JET)# this puts a color efffect on the depth frame
     images = depth_colormap                              # use this for individual streams
-    # cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)   # names and shows the streams
+    cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)   # names and shows the streams
     cv2.imwrite('depthmap.jpg', images)
+
     # # if you press escape or q you can cancel the process
-    # key = cv2.waitKey(1)
-    # print("press escape to cancel")
-    # if key & 0xFF == ord('q') or key == 27:
-    #     cv2.destroyAllWindows()
+    key = cv2.waitKey(1)
+    print("press escape to cancel")
+    if key & 0xFF == ord('q') or key == 27:
+        cv2.destroyAllWindows()
     
 def getDistFromArray(depth_frame_array, bbox):
-    gridSize = PARAMETERS['aiming']['grid_size']
+    """
+    Determines the depth of a bounding box by choosing and filtering the depths of specific points in the bounding box.
+
+    Input: Depth frame and bounding box.
+    Output: Single depth value.
+    """
+    grid_size = PARAMETERS['aiming']['grid_size']
 
     try:
-        xTopLeft = bbox[0] 
-        yTopLeft = bbox[1]
+        x_top_left = bbox[0] 
+        y_top_left = bbox[1]
         width = bbox[2]
         height = bbox[3]
         # this is used to add to the currentX and currentY so that we can get the different points in the 9x9 grid
-        x_interval = width/gridSize
-        y_interval = height/gridSize
+        x_interval = width/grid_size
+        y_interval = height/grid_size
         # stores the x and y of the last point in the grid we got the distance from
-        currX = 0
-        currY = 0
+        curr_x = 0
+        curr_y = 0
         distances = np.array([])
         # double for loop to go through 2D array of 9x9 grid
-        for i in range(gridSize):
-            currX += x_interval # add the interval you calculated to traverse through the 9x9 grid
-            # print(currX)
-            if (xTopLeft+currX >= len(depth_frame_array[0])):
+        for i in range(grid_size):
+            curr_x += x_interval # add the interval you calculated to traverse through the 9x9 grid
+            # print(curr_x)
+            if (x_top_left+curr_x >= len(depth_frame_array[0])):
                 break
-            for j in range(gridSize):
-                currY += y_interval # add the interval you calculated to traverse through the 9x9 grid
+            for j in range(grid_size):
+                curr_y += y_interval # add the interval you calculated to traverse through the 9x9 grid
                 # gets the distance of the point from the depth frame on the grid and appends to the array
-                # print(currY)
-                if (yTopLeft+currY >= len(depth_frame_array)):
+                # print(curr_y)
+                if (y_top_left+curr_y >= len(depth_frame_array)):
                     break
-                distances = np.append(distances, depth_frame_array[int(yTopLeft+currY)][int(xTopLeft+currX)]/1000)
-            currY = 0
+                distances = np.append(distances, depth_frame_array[int(y_top_left+curr_y)][int(x_top_left+curr_x)]/1000)
+            curr_y = 0
         
         distances = distances[distances!=0.0]   # removes all occurances of 0.0 (areas where there is not enough data return 0.0 as depth)
         median = np.median(distances)           # gets the median from the array
         std = np.std(distances)                 # gets the standard deviation from the array
-        modifiedDistances = []                  # initializes a new array for removing outlier numbers
+        modified_distances = []                  # initializes a new array for removing outlier numbers
         # goes through distances array and adds any values that are less than X*standard deviations and ignores the rest
         for i in range(np.size(distances)):
             if abs(distances[i] - median) < 1.5 * std: # tune the standard deviation range for better results
-                modifiedDistances = np.append(modifiedDistances,distances[i])
+                modified_distances = np.append(modified_distances,distances[i])
 
-        distance = (np.mean(modifiedDistances)+np.median(modifiedDistances))/2
+        distance = (np.mean(modified_distances)+np.median(modified_distances))/2
         return distance if (distance and distance>0 and distance<10) else 1
     except:
         return 1
@@ -68,6 +81,12 @@ def getDistFromArray(depth_frame_array, bbox):
 
 # bbox[x coordinate of the top left of the bounding box, y coordinate of the top left of the bounding box, width of box, height of box]
 def WorldCoordinate(depth_frame, bbox):
+    """
+    Returns an estimate in position relative to the world.
+
+    Input: Bounding Box and Depth Frame.
+    Output: 3D point in space.
+    """
     if not depth_frame:                     # if there is no aligned_depth_frame or color_frame then leave the loop
         return None
     # depth_intrin = depth_frame.profile.as_video_stream_profile().intrinsics
@@ -81,13 +100,26 @@ def PixelCoordinate(point):
     return rs.project_point_to_pixel(point)
 
 def bulletDrop(radius, total_angle, proj_velocity, gimbal_pitch, time_interval):    
+    """
+    Determines the bullet offset due to bullet drop and camera offset from shooter.
+    Fails due to not including all variables.
+
+    Input: Depth Amount of Bounding Box.
+    Output: Offset in Pixels.
+    """
     geometric_height = radius * math.sin(total_angle)
     physical_height = proj_velocity * math.sin(gimbal_pitch) * time_interval + 0.5 * -9.8 * time_interval**2
     drop = geometric_height - physical_height   # drop is a POSITIVE VALUE
     return drop
 
 def bulletDropCompensation(depth_image, best_bounding_box, depth_amount, center, phi):
+    """
+    Determines the bullet offset due to bullet drop and camera offset from shooter.
+    Utilizes theory and math.
 
+    Input: Depth Amount of Bounding Box.
+    Output: Offset in Pixels.
+    """
     if phi is None:
         return 0
 
@@ -123,6 +155,13 @@ def bulletDropCompensation(depth_image, best_bounding_box, depth_amount, center,
     return changeP
 
 def bulletDropCompensation2(depth_amount, gimbal_pitch, v_angle, proj_velocity = bullet_velocity):
+    """
+    Determines the bullet offset due to bullet drop and camera offset from shooter.
+    Utilizes theory and math.
+
+    Input: Depth Amount of Bounding Box.
+    Output: Offset in Pixels.
+    """
     barrel_camera_gap = PARAMETERS["aiming"]["barrel_camera_gap"]
     radius = math.sqrt(depth_amount ** 2 + (depth_amount * math.tan(v_angle) + barrel_camera_gap) ** 2)
     total_angle = gimbal_pitch + math.atan2(depth_amount * math.tan(v_angle) + barrel_camera_gap, depth_amount)
@@ -148,7 +187,13 @@ def bulletDropCompensation2(depth_amount, gimbal_pitch, v_angle, proj_velocity =
     return final_gimbal_pitch
 
 def bulletOffsetCompensation(depth_amount):
-    
+    """
+    Determines the bullet offset due to bullet drop and camera offset from shooter.
+    Utilizes a function fitted from varying depth amounts.
+
+    Input: Depth Amount of Bounding Box.
+    Output: Offset in Pixels.
+    """
     if depth_amount > 1 and depth_amount < 5:
         return (1.11208 * depth_amount ** 2) + (.1152 * depth_amount) + (-17.7672)
     else:
