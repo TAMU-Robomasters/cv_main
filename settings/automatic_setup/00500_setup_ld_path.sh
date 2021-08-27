3890cbf9df8f3aca6bf874f9567247c7e21fc023
jeff.hykin <jeff.hykin@gmail.com>
2021-08-27 18:41:13 -0500

    fix .so error

--- settings/automatic_setup/00500_setup_ld_path.sh
+++ settings/automatic_setup/00500_setup_ld_path.sh
@@ -1,2 +1,6 @@
 # add the path to the c lib, which prevents the libstdc++.so.6 errors 
 export LD_LIBRARY_PATH="$("$__PROJECTR_NIX_COMMANDS/lib_path_for" "cc"):$LD_LIBRARY_PATH"
+
+export LD_LIBRARY_PATH="$("$__PROJECTR_NIX_COMMANDS/lib_path_for" libglvnd):$LD_LIBRARY_PATH"
+# export LD_LIBRARY_PATH="$(nix_path_for glib)/lib/:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$("$__PROJECTR_NIX_COMMANDS/lib_path_for" glib):$LD_LIBRARY_PATH"
