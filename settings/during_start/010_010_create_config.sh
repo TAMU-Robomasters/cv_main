3c55f7a47723c6dc8f6c44eba70be21bbb4f2cbe
Jeff Hykin <jeff.hykin@gmail.com>
2022-01-28 09:16:01 -0600

    add auto-creation of local config file

---
+++ settings/during_start/010_010_create_config.sh
@@ -0,0 +1,17 @@
+config_path="$(cat "$FORNIX_FOLDER/main/info.yaml" | yq '.paths.configuration')"
+# 
+# init the config if it doesn't exist
+# 
+if ! [ -f "$config_path" ]
+then
+    echo '
+# Things at the top of the list will override things at the bottom
+# go to ./main/info.yaml look under "configuration" and below the "(default)" and all these options will be listed
+- GPU=NONE          # NONE ,TENSOR_RT, REGULAR
+- BOARD=LAPTOP      # TX2, XAVIER, LAPTOP
+- CAMERA=NONE       # ZED, REALSENSE, NONE
+- MODE=DEVELOPMENT  # DEVELOPMENT, PRODUCTION
+- TEAM=RED          # RED, BLUE
+' > "$config_path"
+
+fi
