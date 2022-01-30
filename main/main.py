8c5c72f09bdc604baa406651ceef9b52f46393e5
Will Curran <willecurran@gmail.com>
2022-01-29 23:12:59 -0600

    Add VideoStream ZED support

--- main/main.py
+++ main/main.py
@@ -137,8 +137,10 @@ def setup(
 
 if __name__ == '__main__':
     # Relative imports here since pyrealsense requires camera to be plugged in or code will crash
-    import videostream._tests.get_live_video_frame as live_video
-    from videostream.realsense import VideoStream
+    if config.hardware.camera == 'zed':
+        from videostream.zed import VideoStream
+    else:
+        from videostream.realsense import VideoStream
     
     video_stream = VideoStream()
     simple_synchronous, synchronous_with_tracker = setup(
