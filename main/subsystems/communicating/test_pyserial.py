c99840e27338ca7dfbb3cb7f8054167334d15115
Xavier 3 <xavier3@example.com>
2022-06-21 20:24:30 -0500

    -

--- main/subsystems/communicating/test_pyserial.py
+++ main/subsystems/communicating/test_pyserial.py
@@ -1,27 +1,49 @@
 import serial
 import time
+# 
+# Xavier3
+# 
 
+from ctypes import *
+
+class Message(Structure):
+    _pack_ = 1
+    _fields_ = [
+        ("magic_number"    , c_uint64  ),
+        ("horizontal_angle", c_float   ),
+        ("vertical_angle"  , c_float   ),
+        ("should_shoot"    , c_uint8   ),
+    ]
+message = Message(0xdeadbeefdeadbef, 0.0, 0.0, 0)
 
 port = serial.Serial(
     "/dev/ttyTHS0",
     baudrate=115200,
-    timeout=.05,
+    timeout=5.5,
     bytesize=serial.EIGHTBITS,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE
 )
 
-
+inc = 0
 while 1:
-    print(f'''----------------------------------------''')
-    print(f'''Writing 1''')
-    port.write(bytes("1\n\0", 'ascii'))
-    time.sleep(5)
+    #print(f'''----------------------------------------''')
+    #print(f'''Writing {bytes(message)}''')
+    # print(f'''sizeof(message) = {sizeof(message)}''')
+    inc += 1
+    #print(f'''inc = {inc}''')
+    # for each in bytes(message):
+    #     print(int(each))
+    message.horizontal_angle += 1
+    port.write(bytes(message))
+    #port.write("a".encode())
+    #print(f'''port.out_waiting = {port.out_waiting}''')
+    time.sleep(0.001)
     
-    print(f'''Reading''')
-    result = port.readline()
-    print(f'''result = {result}''')
-    time.sleep(5)
+    # print(f'''Reading''')
+    # # result = port.readline()
+    # print(f'''result = {result}''')
+    # time.sleep(1)
     
     
     
