1a0aa1dd1cfb57e3179e574d488b7baa9f432a47
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-24 06:55:43 -0500

    add gpu yolov5

--- main/subsystems/modeling/yolo_v5.py
+++ main/subsystems/modeling/yolo_v5.py
@@ -4,6 +4,7 @@ import cv2
 import os
 import time
 from super_map import LazyDict
+import torch
 
 # project imports
 from toolbox.globals import path_to, config, print, runtime
@@ -43,15 +44,17 @@ if should_use_tensor_rt: # tensorRT enabled
     print("RUNNING WITH TENSORRT")
     model.trt_yolo = TrtYOLO(input_shape=(input_dimension, input_dimension), category_num=3, letter_box=False) # what is the 3? -- Jeff
 else:
-    model.net = cv2.dnn.readNetFromDarknet(path_to.model_config, path_to.model_weights)  # init the model
+
     if should_use_gpu:
+        device = torch.device("cuda")
         print("RUNNING WITH GPU ACCELERATION")
-        model.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-        model.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
     else:
-        print("RUNNING WITHOUT GPU ACCELERATION")
-    model.layer_names = model.net.getLayerNames()
-    model.output_layer_names = [model.layer_names[index[0] - 1] for index in model.net.getUnconnectedOutLayers()]
+        device = torch.device("cpu")
+        print("RUNNING ON CPU")
+    
+    model.net = torch.hub.load('ultralytics/yolov5', 'custom', path=path_to.yolov5_pt)
+    model.net.eval()
+    torch.no_grad()
     model.W, model.H = None, None
 
 
@@ -68,18 +71,15 @@ def when_frame_arrives():
         threshold=config.model.threshold,
     )
     
-    screen_center = compute_screen_center(runtime.color_image)
-    
     # best box
     best_bounding_box, current_confidence = get_optimal_bounding_box(
         boxes=boxes,
         confidences=confidences,
-        screen_center=screen_center,
+        screen_center=runtime.screen_center,
         distance=aiming.distance,
     )
     
     # export data
-    runtime.screen_center               = screen_center
     runtime.modeling.boxes              = boxes
     runtime.modeling.confidences        = confidences
     runtime.modeling.best_bounding_box  = best_bounding_box
@@ -91,9 +91,6 @@ def when_frame_arrives():
 # helpers
 # 
 # 
-def compute_screen_center(color_image):
-    return (color_image.shape[1] / 2, color_image.shape[0] / 2)
-
 def get_bounding_boxes(frame, minimum_confidence, threshold):
     # NOTE: this code is derived from https://www.pyimagesearch.com/2018/11/12/yolo-object-detection-with-opencv/
 
@@ -124,44 +121,28 @@ def get_bounding_boxes(frame, minimum_confidence, threshold):
         if model.W is None or model.H is None:
             (model.H, model.W) = frame.shape[:2]
         
-        # convert image to blob before running it in the model
-        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (input_dimension, input_dimension), swapRB=True, crop=False)
-        # provide input and retrive output
-        model.net.setInput(blob)
-        layer_outputs = model.net.forward(model.output_layer_names)
+        layer_outputs = model.net(frame)
+        labels = layer_outputs.xyxy[0]
+        print('labels - ', labels)
 
-        # loop over each of the layer outputs
-        for output in layer_outputs:
-            # loop over each of the detections
-            for detection in output:
-                # extract the class ID and minimum_confidence (i.e., probability)
-                # of the current object detection
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                this_confidence = scores[class_id]
+        # loop over each of the detections
+        for detection in labels:
+            detection = detection.numpy()
+            # extract the class ID and minimum_confidence (i.e., probability)
+            # of the current object detection
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            this_confidence = scores[class_id]
 
-                # filter out weak predictions by ensuring the detected
-                # probability is greater than the minimum probability
-                if this_confidence > minimum_confidence:
-                    # scale the bounding box coordinates back relative to
-                    # the size of the image, keeping in mind that YOLO
-                    # actually returns the center (x, y)-coordinates of
-                    # the bounding box followed by the boxes' width and
-                    # height
-                    box = detection[0:4] * np.array([model.W,model.H, model.W, model.H])
-                    (center_x, center_y, width, height) = box.astype("int")
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if this_confidence > minimum_confidence:
+                # update our list of bounding box coordinates,
+                # confidences, and class IDs
+                boxes.append(detection[0:4])
 
-                    # use the center (x, y)-coordinates to derive the top
-                    # and and left corner of the bounding box
-                    x = int(center_x - (width / 2))
-                    y = int(center_y - (height / 2))
-
-                    # update our list of bounding box coordinates,
-                    # confidences, and class IDs
-                    boxes.append([x, y, int(width), int(height)])
-
-                    confidences.append(float(this_confidence))
-                    class_ids.append(class_id)
+                confidences.append(float(current_confidence))
+                class_ids.append(class_id)
        
     if config.filter_team_color:
         boxes, confidences, class_ids = filter_team(boxes, confidences, class_ids)
@@ -213,9 +194,9 @@ def filter_team(boxes, confidences, class_ids):
     Input: All boxes, confidences, class_ids
     Output: Enemy boxes, confidences, and class_ids
     """
-    enemy_boxes = []
+    enemy_boxes       = []
     enemy_confidences = []
-    enemy_class_ids = []
+    enemy_class_ids   = []
     
     for index in range(len(boxes)):
         if class_ids[index] != color_to_class_id[config.our_team_color]:
@@ -223,5 +204,5 @@ def filter_team(boxes, confidences, class_ids):
             enemy_confidences.append(confidences[index])
             enemy_class_ids.append(class_ids[index])
 
-    # print(enemy_boxes, end=", ")
+    print(enemy_boxes, end=", ")
     return enemy_boxes,enemy_confidences,enemy_class_ids
